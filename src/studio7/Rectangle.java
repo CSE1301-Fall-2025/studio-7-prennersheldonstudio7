3304d73d9405a1cda88d65327bd0229f207018c6
Sofia Prenner <prenner.s@wustl.edu>
2025-11-05 13:57:37 -0600

    rectangle

--- src/studio7/Rectangle.java
+++ src/studio7/Rectangle.java
@@ -6,28 +6,27 @@
 //(This is mainly about the concepts needed to represent a rectangle,
 // but the StdDraw library is included in your repository. 
 //You can also provide a rectangle the ability to draw itself on the screen)
-public class rectangle{
+public class Rectangle{
     private double length;
     private double width;
 
-    public rectangle (double length, double width){
+    public Rectangle (double length, double width){
         this.length = length;
         this.width = width;
     }
-    public static double rectangleArea (double length, double width){
-        double area = length * width;
-        System.out.println("My area is: " + area);
+    public double rectangleArea(){
+        double area = this.length * this.width;
         return area;
     } 
-    public static double rectanglePerimeter (double length, double width){
-        double perimeter = (length * 2) + (width * 2);
+    public double rectanglePerimeter (){
+        double perimeter = (this.length * 2) + (this.width * 2);
         System.out.println("My perimeter is: " + perimeter);
         return perimeter;
     }
-    public static boolean rectangleAreaSmaller(double length, double width, double otherLength, double otherWidth){
-        double otherArea = otherLength * otherWidth;
-        double area = rectangleArea(length, width);
-        if area < otherArea{
+    public boolean rectangleAreaSmaller(Rectangle r2){
+        double otherArea = r2.rectangleArea();
+        double area = this.rectangleArea();
+        if (area > otherArea){
             return true;
         }
         else{
@@ -43,7 +42,10 @@ public class rectangle{
         }
     }
     public static void main(String[] args){
-        rectangle r1 = new rectangle(4,5);
-        System.out.print("My area is: " + r1.rectangleArea);
+        Rectangle r1 = new Rectangle(4,5);
+        System.out.println("My area is: " + r1.rectangleArea());
+        Rectangle r2 = new Rectangle(2,3);
+        System.out.println("Rectangle 1 is larger than rectangle 2: " + r1.rectangleAreaSmaller(r2));
     }
 }
+
